9044da1a0aca3a1f8753167faf564d67a0b348de
Mark Chambers <me@marksy.com>
2018-06-07 10:04:56 +1200

    removed superflueous code

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -1,54 +1,17 @@
 import React from 'react'
 import profileImage from '../assets/profile.jpg'
-import dealwithit from '../assets/dealwithit.png'
 
-const initialState = {
-  mattjamesishere: false,
-  name: 'Mark Chambers'
-}
-
-let counter = 0
-
-class Home extends React.Component {
-  constructor() {
-    super()
-
-    this.state = initialState
-
-    this.mattjames = this.mattjames.bind(this)
-    this.hideMattjames = this.hideMattjames.bind(this)
-  }
-
-  mattjames() {
-    counter += 1;
-    if (counter === 7) {
-      this.setState((prevState) => ({
-        mattjamesishere: true,
-        name: 'Matt James'
-      }));
-    }
-  }
-
-  hideMattjames() {
-    counter = 0;
-    this.setState(initialState);
-  }
-
-  render() {
-    return (
-      <section className='home'>
-        <div className='hero'>
-          <p className='noselect'>
-            Hullo. My name is {this.state.name}.<br/> I'm a Front End Developer in Wellington, NZ.
-          </p>
-        </div>
-    	  <div className='profile'>
-    	  	<img src={profileImage} id='profpic' alt=''/>
-    		  <img src={dealwithit} className='dealwithit' alt=''/>
-    	  </div>
-      </section>
-    )
-  }
-}
+const Home = () => (
+  <section className='home'>
+    <div className='hero'>
+      <p className='noselect'>
+        Hullo. My name is Mark Chambers.<br/> I'm a UI Developer in Wellington, NZ.
+      </p>
+    </div>
+	  <div className='profile'>
+	  	<img src={profileImage} alt=''/>
+	  </div>
+  </section>
+);
 
 export default Home;
--- website/src/components/Job.js
+++ website/src/components/Job.js
@@ -6,7 +6,6 @@ class Job extends React.Component {
       <li>
         <h2>{this.props.details.name}</h2>
         <p className='date'>{this.props.details.date}</p>
-        {/*}<p className='desc'>{this.props.details.desc}</p>*/}
       </li>
     )
   }
